a86832bc93cc54845418abdb48c8cfc436ef549b
elipmoc <relipmoc101@gmail.com>
2017-07-09 14:51:48 +0900

    Scalaやりたい。でも時間取れない。悲しみ

--- AnnihilationCharacter/AnnihilationCharacter/main.cpp
+++ AnnihilationCharacter/AnnihilationCharacter/main.cpp
@@ -4,11 +4,16 @@
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//よくある初期化
 	ChangeWindowMode(TRUE);
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
+
+	//最初に呼ばれるシーンをセット
 	auto titleScene = new game::TitleScene();
 	generic::SceneManager::GetInstance()->SetScene(titleScene);
+
+	//はいはいループループ
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
 		generic::SceneManager::GetInstance()->SceneUpdate();
 	}

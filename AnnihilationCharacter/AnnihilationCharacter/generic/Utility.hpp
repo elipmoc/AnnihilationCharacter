fb0deb51a1105df72ace2f3d4d8f810fdea18657
elipmoc <relipmoc101@gmail.com>
2017-07-11 14:03:06 +0900

    ラジアンを度数法に直す関数つくったった

--- AnnihilationCharacter/AnnihilationCharacter/generic/Utility.hpp
+++ AnnihilationCharacter/AnnihilationCharacter/generic/Utility.hpp
@@ -3,8 +3,16 @@ namespace generic{
 	//初めて変数テンプレート作った！！！いえーい
 	template <class T>
 	constexpr T pi = static_cast<T>(3.14159265358979323846);
+
+	//ラジアン値に変換
 	template<class T>
 	constexpr T ToRad(T deg) {
-		return deg*pi<T> / 180;
+		return (deg*pi<T>)/ 180;
+	}
+
+	//度数値に変換
+	template<class T>
+	constexpr T ToDeg(T rad) {
+		return (rad*180)/pi<T>;
 	}
 }
